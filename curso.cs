fe2095462382bdcdcb223e2705cae278e1492b41
MATÍAS ACUÑA COCCOLA <50233630@ort>
2026-03-26 08:05:10 -0300

    diccionario

--- curso.cs
+++ curso.cs
@@ -1,45 +1,41 @@
 public class Curso
 {
-    public List<Alumno> alumnos = new List<Alumno>();
-
+      public Dictionary<int, Alumno> alumnos = new Dictionary<int, Alumno>();
 
     public void AgregarAlumno(Alumno alumno)
-{
-    if (BuscarAlumno(alumno.DNI) == null)
     {
-        alumnos.Add(alumno);
-        Console.WriteLine("Alumno agregado correctamente.");
+    
+        if (!alumnos.ContainsKey(alumno.DNI))
+        {
+           
+            alumnos.Add(alumno.DNI, alumno); 
+            Console.WriteLine("Alumno agregado correctamente.");
+        }
+        else
+        {
+            Console.WriteLine("Error. Ya existe un alumno con ese DNI.");
+        }
     }
-    else
-    {
-        Console.WriteLine("Error. ya existe un alumno con ese DNI.");
-    }
-}
-
 
     public Alumno BuscarAlumno(int dni)
     {
-        foreach (Alumno a in alumnos)
-        {
-            if (a.DNI == dni)
-                return a;
-        }
+      
+        if (alumnos.ContainsKey(dni))
+            return alumnos[dni]; 
         return null;
     }
 
-
     public void MostrarAlumnos()
     {
-        foreach (Alumno a in alumnos)
+          foreach (Alumno a in alumnos.Values)
         {
             a.Mostrar();
         }
     }
 
-
     public void MostrarLibres()
     {
-        foreach (Alumno a in alumnos)
+        foreach (Alumno a in alumnos.Values)
         {
             if (a.CantidadFaltas > 15)
             {
